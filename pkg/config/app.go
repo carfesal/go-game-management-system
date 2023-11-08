e0182bc1c04802529551810958dd34edfdf7a9ea
Carlos Salazar Moreira <56647144+carfesal@users.noreply.github.com>
2023-11-08 09:44:26 -0500

    feature: connection w database (sqlite)

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -1,1 +1,22 @@
 package config
+
+import (
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB
+
+func ConnectDB() {
+	database, err := gorm.Open(sqlite.Open("games.db"), &gorm.Config{})
+
+	if err != nil {
+		panic("Oops! Something went wrong while connecting to the database!")
+	}
+
+	db = database
+}
+
+func GetDB() *gorm.DB {
+	return db
+}
